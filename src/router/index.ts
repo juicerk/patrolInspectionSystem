214c1a12e04e5199c1c4061329cee98e72104202
juicer <2834839551@qq.com>
2024-09-13 19:17:00 +0800

    master

---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import setupGuards from './guards'
+
+// 路由相关数据
+import { constantRoutes, constantRoutesByFilesystem } from './routes'
+import pinia from '@/store'
+import useSettingsStore from '@/store/modules/settings'
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: useSettingsStore(pinia).settings.app.routeBaseOn === 'filesystem' ? constantRoutesByFilesystem : constantRoutes,
+})
+
+setupGuards(router)
+
+export default router
